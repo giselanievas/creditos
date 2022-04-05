7dbadb2893146d5e87e87b02285584a363a5cac1
francop16 <fputrino@gmail.com>
2022-04-05 10:21:30 -0300

    agregar inicio para modificarse

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>pantalla de inicio</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \APP\InicioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [App\Http\Controllers\InicioController::class, 'index'])->name('inicio');
 
 Auth::routes();
 
