a622a69ed3fcd9f23e022ee8a1c2b5b163304728
francop16 <fputrino@gmail.com>
2022-05-05 09:01:38 -0300

    estilos de login y registros

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header" style="background-color: rgb(80, 20, 120); color: white">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     @if(Session::has('msg'))
@@ -22,7 +22,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-end">E-Mail:</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror rounded-pill shadow-sm " name="email" value="{{ old('email') }} " required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -36,7 +36,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}:</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror rounded-pill shadow-sm " name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -59,13 +59,13 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12 offset-md-3">
+                                <button type="submit" style="background-color: rgb(00, 235, 185); color: white" class="btn rounded-pill col-3">
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link" style="color: rgb(80, 20, 120)" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
