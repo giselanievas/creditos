2584f4a723401cb4a7eed2e8179673152c279e04
francop16 <desarrolloswebjunin@gmail.com>
2022-06-10 10:04:08 -0300

    config para produccion4

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
     {
         return $next($request)
             //Url a la que se le dará acceso en las peticiones
-            ->header("Access-Control-Allow-Origin", "https://plautocreditos.com.ar/ingreso")
+            ->header("Access-Control-Allow-Origin", "https://plautocreditos.com.ar/lineacredito")
             //Métodos que a los que se da acceso
             ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
             //Headers de la petición
