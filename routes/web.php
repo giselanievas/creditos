5d8459f3cff95e1521a17c51901a5bbebcf1864f
francop16 <desarrolloswebjunin@gmail.com>
2022-06-10 10:01:52 -0300

    config para produccion2

--- routes/web.php
+++ routes/web.php
@@ -25,48 +25,55 @@ Route::get('/registrado', [App\Http\Controllers\InicioController::class, 'regist
 Route::get('/validar/{id}', [App\Http\Controllers\InicioController::class, 'validar'])->name('validarEmail');
 Route::get('/validado', [App\Http\Controllers\InicioController::class, 'validado'])->name('validado');
 
-////////////////////////////////////////////  Tipo  de linea de creditos /////////////////////////////////////////////////////////////////
+ //Rutas a las que se permitirá acceso
+Route::group(['middleware' => ['cors']], function () {
+   
+        ////////////////////////////////////////////  Tipo  de linea de creditos /////////////////////////////////////////////////////////////////
 
-Route::get('/tipodelinea', [App\Http\Controllers\CreditoController::class, 'index'])->name('tipoLineaCredito');
-Route::post('/tipodelinea', [App\Http\Controllers\CreditoController::class, 'agregar'])->name('agregarTipoLinea');
-Route::get('/tipodelinea/editar/{id}', [App\Http\Controllers\CreditoController::class, 'editar'])->name('editarTipoDeLinea');
-Route::put('/tipodelinea/editar/{id}', [App\Http\Controllers\CreditoController::class, 'update'])->name('updateTipoDeLinea');
-Route::get('/tipodelinea/habilitar/{id}', [App\Http\Controllers\CreditoController::class, 'habilitar'])->name('habilitarTipoLinea');
-Route::get('/tipodelinea/eliminar/{id}', [App\Http\Controllers\CreditoController::class, 'eliminar'])->name('eliminaTipoDeLinear');
-
-
-////////////////////////////////////////////  Linea de creditos /////////////////////////////////////////////////////////////////
-Route::get('/inicioCreditos', [App\Http\Controllers\LineaCreditoController::class, 'inicio'])->middleware('can:lineaCredito')->name('inicioCreditos');
-Route::get('/lineacredito', [App\Http\Controllers\LineaCreditoController::class, 'index'])->middleware('can:lineaCredito')->name('lineaCredito');
-Route::get('/lineacredito/agregar', [App\Http\Controllers\LineaCreditoController::class, 'agregar'])->name('agregarLineaCredito');
-Route::post('/lineacredito/agregar', [App\Http\Controllers\LineaCreditoController::class, 'guardar'])->name('guardarLineaCredito');
-Route::get('/lineacredito/editar/{id}', [App\Http\Controllers\LineaCreditoController::class, 'editar'])->name('editarLinea');
-Route::put('/lineacredito/editar/{id}', [App\Http\Controllers\LineaCreditoController::class, 'update'])->name('updateLinea');
-//**********************************Grilla de creditos***********************************************************////
-Route::get('/lineacredito/agregargrilla', [App\Http\Controllers\ImagenesController::class, 'index'])->name('grillaDeCredito');
-Route::post('/lineacredito/guardargrilla', [App\Http\Controllers\ImagenesController::class, 'create'])->name('guardarGrillaDeCredito');
+        Route::get('/tipodelinea', [App\Http\Controllers\CreditoController::class, 'index'])->name('tipoLineaCredito');
+        Route::post('/tipodelinea', [App\Http\Controllers\CreditoController::class, 'agregar'])->name('agregarTipoLinea');
+        Route::get('/tipodelinea/editar/{id}', [App\Http\Controllers\CreditoController::class, 'editar'])->name('editarTipoDeLinea');
+        Route::put('/tipodelinea/editar/{id}', [App\Http\Controllers\CreditoController::class, 'update'])->name('updateTipoDeLinea');
+        Route::get('/tipodelinea/habilitar/{id}', [App\Http\Controllers\CreditoController::class, 'habilitar'])->name('habilitarTipoLinea');
+        Route::get('/tipodelinea/eliminar/{id}', [App\Http\Controllers\CreditoController::class, 'eliminar'])->name('eliminaTipoDeLinear');
 
 
-
-//////////////////////////////////////////// Detalle Linea de creditos /////////////////////////////////////////////////////////////////
-Route::get('/detalleLinea', [App\Http\Controllers\DetalleCreditoController::class, 'index'])->middleware('can:detalleLineaCredito')->name('detalleLineaCredito');
-Route::get('/detalleLinea/agregar', [App\Http\Controllers\DetalleCreditoController::class, 'agregar'])->name('agregarDetalleLinea');
-Route::post('/detalleLinea/agregar', [App\Http\Controllers\DetalleCreditoController::class, 'guardar'])->name('guardarDetalleLinea');
-Route::get('/detalleLinea/editar/{id}', [App\Http\Controllers\DetalleCreditoController::class, 'editar'])->name('editarDetalleDeLinea');
-Route::put('/detalleLinea/editar/{id}', [App\Http\Controllers\DetalleCreditoController::class, 'update'])->name('updateDetalleDeLinea');
-Route::get('/detalleLinea/habilitar/{id}', [App\Http\Controllers\DetalleCreditoController::class, 'habilitar'])->name('habilitarDetalleLinea');
-Route::get('/detalleLinea/eliminar/{id}', [App\Http\Controllers\DetalleCreditoController::class, 'eliminar'])->name('bajaDetalleDeLinear');
-
-////////////////////////////////////////// cotizado con VUE ///////////////////////////////////////////////////////////////////////////////
-Route::get('/cotizador', [App\Http\Controllers\CotizadorController::class, 'index']);
-Route::post('/cotizador/{id}/{modelo}/{cuotas}', [App\Http\Controllers\CotizadorController::class, 'calculos']);
-
-/////////////////////////////////////////formulario////////////////////////////////////////////////////////////////////////////////////////
-
-Route::post('/formulario', [App\Http\Controllers\FormularioController::class,'store'])->name('formulario');
+        ////////////////////////////////////////////  Linea de creditos /////////////////////////////////////////////////////////////////
+        Route::get('/inicioCreditos', [App\Http\Controllers\LineaCreditoController::class, 'inicio'])->middleware('can:lineaCredito')->name('inicioCreditos');
+        Route::get('/lineacredito', [App\Http\Controllers\LineaCreditoController::class, 'index'])->middleware('can:lineaCredito')->name('lineaCredito');
+        Route::get('/lineacredito/agregar', [App\Http\Controllers\LineaCreditoController::class, 'agregar'])->name('agregarLineaCredito');
+        Route::post('/lineacredito/agregar', [App\Http\Controllers\LineaCreditoController::class, 'guardar'])->name('guardarLineaCredito');
+        Route::get('/lineacredito/editar/{id}', [App\Http\Controllers\LineaCreditoController::class, 'editar'])->name('editarLinea');
+        Route::put('/lineacredito/editar/{id}', [App\Http\Controllers\LineaCreditoController::class, 'update'])->name('updateLinea');
+        //**********************************Grilla de creditos***********************************************************////
+        Route::get('/lineacredito/agregargrilla', [App\Http\Controllers\ImagenesController::class, 'index'])->name('grillaDeCredito');
+        Route::post('/lineacredito/guardargrilla', [App\Http\Controllers\ImagenesController::class, 'create'])->name('guardarGrillaDeCredito');
 
 
 
-//////////////////////////////////////////////Ingreso Logueado///////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////// Detalle Linea de creditos /////////////////////////////////////////////////////////////////
+        Route::get('/detalleLinea', [App\Http\Controllers\DetalleCreditoController::class, 'index'])->middleware('can:detalleLineaCredito')->name('detalleLineaCredito');
+        Route::get('/detalleLinea/agregar', [App\Http\Controllers\DetalleCreditoController::class, 'agregar'])->name('agregarDetalleLinea');
+        Route::post('/detalleLinea/agregar', [App\Http\Controllers\DetalleCreditoController::class, 'guardar'])->name('guardarDetalleLinea');
+        Route::get('/detalleLinea/editar/{id}', [App\Http\Controllers\DetalleCreditoController::class, 'editar'])->name('editarDetalleDeLinea');
+        Route::put('/detalleLinea/editar/{id}', [App\Http\Controllers\DetalleCreditoController::class, 'update'])->name('updateDetalleDeLinea');
+        Route::get('/detalleLinea/habilitar/{id}', [App\Http\Controllers\DetalleCreditoController::class, 'habilitar'])->name('habilitarDetalleLinea');
+        Route::get('/detalleLinea/eliminar/{id}', [App\Http\Controllers\DetalleCreditoController::class, 'eliminar'])->name('bajaDetalleDeLinear');
 
-Route::get('/ingreso', [App\Http\Controllers\IngresoController::class, 'index'])->name('ingreso');
+        ////////////////////////////////////////// cotizado con VUE ///////////////////////////////////////////////////////////////////////////////
+        Route::get('/cotizador', [App\Http\Controllers\CotizadorController::class, 'index']);
+        Route::post('/cotizador/{id}/{modelo}/{cuotas}', [App\Http\Controllers\CotizadorController::class, 'calculos']);
+
+        /////////////////////////////////////////formulario////////////////////////////////////////////////////////////////////////////////////////
+
+        Route::post('/formulario', [App\Http\Controllers\FormularioController::class,'store'])->name('formulario');
+
+
+
+        //////////////////////////////////////////////Ingreso Logueado///////////////////////////////////////////////////////////////////////////////
+
+        Route::get('/ingreso', [App\Http\Controllers\IngresoController::class, 'index'])->name('ingreso');
+
+
+
+});
